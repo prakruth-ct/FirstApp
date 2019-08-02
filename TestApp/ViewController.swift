ef6234b44d587d3fcd8fc48d96e6f6c1d9017c38
prakruth-yml <prakruth.nagaraj@ymedialab.com>
2019-08-02 18:26:22 +0530

    First Commit

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func showAlertWhenButtonHit(){
+        
+        let alert = UIAlertController(title: "First App", message: "My First App", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
