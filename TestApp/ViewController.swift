bbcdccd02d884ab48119d063895bce4602bebab1
prakruth-yml <prakruth.nagaraj@ymedialab.com>
2019-08-06 18:15:13 +0530

    Aug 06 Commit

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var slider : UISlider?
+    @IBOutlet weak var targetLabel: UILabel?
+    @IBOutlet weak var scoreLabel: UILabel?
+    @IBOutlet weak var roundLabel: UILabel?
+    
+    var target : Int = 0
+    var round: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,36 +25,42 @@ class ViewController: UIViewController {
             print("slider Init value \(sliderObj.value.rounded())")
         }
         else{
-            let alert = UIAlertController(title: "Error", message: "Restart App", preferredStyle: .alert)
-            let action = UIAlertAction(title: "Close", style: .default, handler: nil)
-            alert.addAction(action)
-            present(alert, animated: true, completion: nil)
+            showAlert(msg: "Restart App")
         }
+        round = 0
+        newRound()
     }
 
     @IBAction func showAlertWhenButtonHit(){
         
         print("Button 1 Pressed")
-        let alert = UIAlertController(title: "First App", message: "My First App", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
+        //showAlert(msg: "\(target) \(slider?.value.rounded() ?? 0.0)")
+        newRound()
         
     }
     
-//    @IBAction func showAlert(msg : String){
-//
-//        let alert = UIAlertController(title: "Error", message: msg, preferredStyle: .alert)
-//        let action = UIAlertAction(title: "Close", style: .default, handler: nil)
-//        alert.addAction(action)
-//        present(alert, animated: true, completion: nil)
-//    }
+    func showAlert(msg : String){
+
+        let alert = UIAlertController(title: "Error", message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Close", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
     
     @IBAction func sliderAction(slider : UISlider){
         
         print("Slider value \(slider.value)")
         let sliderValue = slider.value.rounded()
         
+    }
+    
+    func newRound(){
+        target = Int.random(in: 1...100)
+        slider?.value = 50.0
+        round += 1
+//        roundLabel?.value(forKey: String(round))
+//        scoreLabel?.value(forKey: String(320))
+//        targetLabel?.value(forKey: String(target))
     }
 
 }
